f72b40f6fe459f12d98c2e27b170ab9ed56bc551
unknown <mehedytouhidurzaman@gmail.com>
2024-07-28 20:48:43 +0600

    Hero section completed with responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,21 @@
+import bookImage from "../assets/Images/pngwing 1.png"
+
 const Home = () => {
     return (
       <div className="container mx-auto">
         <div>
-          <h1>This is Home page</h1>
+          {/* Hero section */}
+          <div className="flex flex-col-reverse md:flex-row justify-center md:justify-evenly md:items-center text-center md:text-justify p-6 bg-[#1313130D] rounded-lg ">
+            <div className="space-y-10">
+              <h1 className="font-bold text-5xl text-black">
+                Books to freshen <br /> up your bookshelf
+              </h1>
+              <button className="bg-[#23BE0A] px-2 py-3 rounded-lg">View the list</button>
+            </div>
+            <div>
+              <img src={bookImage} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     );
